8e9eace15e16ecb3f1eef2a6c5b2d3248c3d4f10
oawood93 <oa.wood@outlook.com>
2022-01-24 15:52:03 +0000

    red - green - red

--- src/main/java/LeapYear/example/LeapYear.java
+++ src/main/java/LeapYear/example/LeapYear.java
@@ -3,7 +3,7 @@ package LeapYear.example;
 public class LeapYear {
 
     public boolean check(int i) {
-        if (i % 100 == 0)
+        if (i % 100 == 0 && i % 400 != 0)
             return false;
         if (i % 4 == 0 && i != 0)
             return true;
--- src/test/java/LeapYearShould.java
+++ src/test/java/LeapYearShould.java
@@ -1,4 +1,5 @@
 import LeapYear.example.LeapYear;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -7,10 +8,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LeapYearShould {
 
+    private LeapYear leapYear;
+
+    @BeforeEach
+    public void SetUp(){
+        leapYear = new LeapYear();
+    }
     @Test
     public void
     return_false_when_passed_0() {
-        var leapYear = new LeapYear();
         var result = leapYear.check(0);
         assertFalse(result);
     }
@@ -21,7 +27,6 @@ public class LeapYearShould {
                 "12, true"
     })
     void return_true_when_divisible_by_4(int input, boolean expectedOutput){
-        var leapYear = new LeapYear();
         var result = leapYear.check(input);
 
         assertEquals(expectedOutput, result);
@@ -29,28 +34,24 @@ public class LeapYearShould {
 
     @Test
     void return_false_when_passed_100() {
-        var leapYear = new LeapYear();
         var result = leapYear.check(100);
         assertFalse(result);
     }
 
     @Test
     void return_false_when_passed_200() {
-        var leapYear = new LeapYear();
         var result = leapYear.check(200);
         assertFalse(result);
     }
 
     @Test
     void return_false_when_passed_300() {
-        var leapYear = new LeapYear();
         var result = leapYear.check(300);
         assertFalse(result);
     }
 
     @Test
     void return_true_when_passed_400() {
-        var leapYear = new LeapYear();
         var result = leapYear.check(400);
         assertTrue(result);
     }
