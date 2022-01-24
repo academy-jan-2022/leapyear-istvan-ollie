b5ea2f2ae832ced06fa4c682693525288d477976
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-01-24 16:32:10 +0100

    green

--- src/main/java/LeapYear/example/LeapYear.java
+++ src/main/java/LeapYear/example/LeapYear.java
@@ -3,7 +3,10 @@ package LeapYear.example;
 public class LeapYear {
 
     public boolean check(int i) {
-        if (i % 4 == 0 && i != 0) return true;
+        if (i == 100)
+            return false;
+        if (i % 4 == 0 && i != 0)
+            return true;
         return false;
     }
 }
