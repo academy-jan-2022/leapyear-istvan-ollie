82215aab98a33688bb5b462300b4b86eedcc1cbb
iagardi <garbigz@gmail.com>
2022-01-24 15:21:03 +0000

    green

--- src/main/java/LeapYear/example/LeapYear.java
+++ src/main/java/LeapYear/example/LeapYear.java
@@ -3,7 +3,7 @@ package LeapYear.example;
 public class LeapYear {
 
     public boolean check(int i) {
-        if (i == 4 || i == 8) return true;
+        if (i % 4 == 0 && i != 0) return true;
         return false;
     }
 }
