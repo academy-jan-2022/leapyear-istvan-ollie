75b8066ae8cd593ff59a6910bf590031742d397e
oawood93 <oa.wood@outlook.com>
2022-01-24 14:09:11 +0000

    red

--- src/test/java/LeapYearShould.java
+++ src/test/java/LeapYearShould.java
@@ -2,6 +2,7 @@ import LeapYear.example.LeapYear;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearShould {
     @Test
@@ -10,5 +11,13 @@ public class LeapYearShould {
         var leapYear = new LeapYear();
         var result = leapYear.check(0);
         assertFalse(result);
+    }
+
+    @Test
+    public void
+    return_true_when_passed_4(){
+        var leapYear = new LeapYear();
+        var result = leapYear.check(4);
+        assertTrue(result);
     }
 }
