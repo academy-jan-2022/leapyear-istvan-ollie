d392b288848dd0e16d313b1d351f774773ac17ce
iagardi <garbigz@gmail.com>
2022-01-24 15:30:41 +0000

    red

--- src/test/java/LeapYearShould.java
+++ src/test/java/LeapYearShould.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearShould {
+
     @Test
     public void
     return_false_when_passed_0() {
@@ -12,7 +13,6 @@ public class LeapYearShould {
         var result = leapYear.check(0);
         assertFalse(result);
     }
-
     @Test
     void return_true_when_passed_8() {
         var leapYear = new LeapYear();
@@ -35,4 +35,12 @@ public class LeapYearShould {
         var result = leapYear.check(12);
         assertTrue(result);
     }
+
+    @Test
+    void return_false_when_passed_100() {
+        var leapYear = new LeapYear();
+        var result = leapYear.check(100);
+        assertFalse(result);
+    }
+
 }
